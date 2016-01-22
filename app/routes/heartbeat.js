044d426462e8ac82c5e1b837d269a8aca34934e2
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-22 03:47:01 -0800

    Update heartbeat copy.

--- app/routes/heartbeat.js
+++ app/routes/heartbeat.js
@@ -2,6 +2,9 @@ export default {
   method: "GET",
   path: "/heartbeat",
   handler(request, reply) {
-    return reply("hello world")
+    return reply({
+      message: `${(new Date()).toLocaleTimeString()}: Still alive :)`,
+      statusCode: 200
+    })
   }
 }
