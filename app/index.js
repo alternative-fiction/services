14431adc8d088577ff6760bfc34cc4d7f2637c83
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-23 07:27:24 -0800

    Adjust server init for testing.

--- app/index.js
+++ app/index.js
@@ -6,23 +6,22 @@ import {Server} from "hapi"
 import routes from "./routes"
 import plugins from "./plugins"
 
-export const server = new Server()
+const server = new Server()
 
-exports.init = function init() {
-  server.connection({
-    host: "localhost",
-    port: 9001
-  })
+server.connection({
+  host: "localhost",
+  port: 9001
+})
 
-  server.route(routes)
+server.route(routes)
 
-  server.register(plugins, error => {
-    if (error) throw error
+server.register(plugins, error => {
+  if (error) throw error
+})
 
-    server.start(() => {
-      console.log("\n\n===========".green)
-      console.log(`${(new Date()).toLocaleTimeString()}: Listening at ${server.info.uri}`)
-      console.log("===========\n\n".blue)
-    })
-  })
-}
+server.initialize(error => {
+  if (error) throw error
+  console.log("Initialized")
+})
+
+export default server
--- scripts/watch.js
+++ scripts/watch.js
@@ -1,2 +1,7 @@
-require("babel-register")
-require("../app").init()
+import server from "../app"
+
+server.start(() => {
+  console.log("\n\n===========".green)
+  console.log(`${(new Date()).toLocaleTimeString()}: Listening at ${server.info.uri}`)
+  console.log("===========\n\n".blue)
+})
