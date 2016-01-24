b8925ac085220b9b8d61fa6a8d49dd208cef80f3
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-24 04:13:31 -0800

    Don't log to file in production.

--- app/plugins/good.js
+++ app/plugins/good.js
@@ -1,14 +1,22 @@
+const reporters = [
+  {
+    reporter: require("good-console"),
+    events: {log: "*", response: "*"}
+  }
+]
+
+if (process.env.NODE_ENV !== "production") {
+  reporters.push({
+    reporter: require("good-file"),
+    events: {log: "*", response: "*"},
+    config: "./tmp/logs.txt"
+  })
+}
+
 export default {
   register: require("good"),
   options: {
     opsInterval: 1000,
-    reporters: [{
-      reporter: require("good-console"),
-      events: {log: "*", response: "*"}
-    }, {
-      reporter: require("good-file"),
-      events: {log: "*", response: "*"},
-      config: "./tmp/logs.txt"
-    }]
+    reporters
   }
 }
