34b12135330715d83eb36bf5560f3e3309587b00
Teffen Ellis (Eric) <teffen@nirri.us>
2016-01-30 15:38:29 +0900

    Simplify unique ID helper.

--- app/lib/unique-id.js
+++ app/lib/unique-id.js
@@ -1,11 +1,8 @@
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+import Chance from "chance"
+
+const chance = new Chance()
+const pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
 export default function uniqueId(length = 12) {
-  let guid = ""
-
-  for (let i = 0; i < length; i++) {
-    guid += characters.charAt(Math.floor(Math.random() * characters.length))
-  }
-
-  return guid
+  return chance.string({length, pool})
 }
