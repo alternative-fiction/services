b9747582961e94cf22739661715e26c66828cb67
Teffen Ellis (Eric) <teffen@nirri.us>
2016-03-03 02:04:03 -0800

    Update babel, eslint.
    - Fix linter errors.

--- migrations/20160121012125_ci_text.js
+++ migrations/20160121012125_ci_text.js
@@ -1,9 +1,9 @@
-"use strict";
+"use strict"
 
 exports.up = function(knex) {
-  return knex.raw(`CREATE EXTENSION IF NOT EXISTS citext;`)
+  return knex.raw("CREATE EXTENSION IF NOT EXISTS citext;")
 }
 
 exports.down = knex => {
-  return knex.raw(`DROP EXTENSION IF EXISTS citext;`)
+  return knex.raw("DROP EXTENSION IF EXISTS citext;")
 }
